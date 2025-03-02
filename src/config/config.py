972fde0a4b994ca7199b88da20183d7fcc57ac08
Alyce Thomas <80847508+alycet@users.noreply.github.com>
2025-03-02 14:24:09 -0500

    Add files via upload

---
+++ src/config/config.py
@@ -0,0 +1,18 @@
+config = {
+    "openai": {
+        "api_key" : "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"
+    },
+    "kafka": {
+        "sasl.username": "2IHQECNGSDBRKZEP",
+        "sasl.password": "b38edo0wdapjQ4jLrPJvz4S/qFbE/aC4W7C58ddiciWJFT+HmISS39J9xBZnjQBf",
+        "bootstrap.servers": "pkc-p11xm.us-east-1.aws.confluent.cloud:9092",
+        "security.protocol": "SASL_SSL",
+        "sasl.mechanism": "PLAIN",
+        "session.timeout.ms" : 50000
+    },
+    "schema_registry": {
+        "url": "https://psrc-6kq702.us-east-1.aws.confluent.cloud",
+        "basic.auth.user.info": "ZKZNPADH42TZ5XY7:75yltgepHmhLob3hYFEBrs7OaNa7bvhyJbmVT/LR2IvPEdSPVP1WnEdZVJXIwYKX"
+    }
+    
+}
